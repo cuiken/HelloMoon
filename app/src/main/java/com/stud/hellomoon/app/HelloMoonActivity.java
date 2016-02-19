11c6b18c9abdd3ca20c88f1182c4d397503878a8
cuiken <phckren@163.com>
2016-02-19 13:59:39 +0800

    --国际化--

--- app/src/main/java/com/stud/hellomoon/app/HelloMoonActivity.java
+++ app/src/main/java/com/stud/hellomoon/app/HelloMoonActivity.java
@@ -2,6 +2,7 @@ package com.stud.hellomoon.app;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
 
 public class HelloMoonActivity extends FragmentActivity {
 
@@ -9,5 +10,12 @@ public class HelloMoonActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_moon);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_hello_moon, menu);
+        return true;
     }
 }
